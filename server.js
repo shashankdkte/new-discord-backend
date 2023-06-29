2e3466c092ffba66ac5c342905bb80dbf9aa9252
Shashank Dhakate <shashankdkte@gmail.com>
2023-06-29 18:24:53 +0530

    Adding Authentication routes

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const http = require("http");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const { error } = require("console");
+
+const authRoute = require("./routes/authRoute")
+
 require("dotenv").config();
+
 
 const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/auth", authRoute);
 
 const server = http.createServer(app);
 
