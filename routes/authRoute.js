480584be0c69a0ed118c9e2512af8426af875b5e
Shashank Dhakate <shashankdkte@gmail.com>
2023-06-29 19:01:51 +0530

    Adding Request Validation with Joi

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,25 @@
 const express = require("express");
-const authController = require("./../controllers/auth/authController")
 const router = express.Router();
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
+const authController = require("./../controllers/auth/authController")
 
-router.post("/register",authController.controllers.postRegister)
-router.post("/login",authController.controllers.postLogin)
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(12).required(),
+  password: Joi.string().min(6).max(12).required(),
+  mail:Joi.string().email().required()
+})
+
+const loginSchema = Joi.object({
+  password: Joi.string().min(6).max(12).required(),
+  mail:Joi.string().email().required()
+})
+
+
+
+
+router.post("/register",validator.body(registerSchema), authController.controllers.postRegister)
+router.post("/login", validator.body(loginSchema),authController.controllers.postLogin)
 
 
 module.exports = router;
