37b9ab82177b004554928e157b74e5ade45bb25e
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-08 16:32:23 +0530

    Online Indicator

--- serverStore.js
+++ serverStore.js
@@ -34,10 +34,20 @@ const getActiveConnections = (userId) => {
   return activeConnections;
 }
 
+
+const getOnlineUsers = () => {
+  const onlineUsers = [];
+  connectedUsers.forEach((value, key) => {
+    onlineUsers.push({socketId:key, userId:value.userId})
+  })
+  return onlineUsers
+}
+
 module.exports = {
   addNewConnectedUser,
   removeConnectedUser,
   getActiveConnections,
   getSocketInstance,
-  setSocketInstance
+  setSocketInstance,
+  getOnlineUsers
 }
