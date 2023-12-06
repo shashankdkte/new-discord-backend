c6767d331d5be1cfaf7cebb4fb6313ee4b22ac1e
Shashank Dhakate <shashankdkte@gmail.com>
2023-12-06 18:55:27 +0530

    Preparing for incoming WebRTC Connection

--- socketHandler/roomJoinHandler.js
+++ socketHandler/roomJoinHandler.js
@@ -11,6 +11,18 @@ const roomJoinHandler = (socket, data) => {
 
   const roomDetails = serverStore.getActiveRoom(roomId);
   serverStore.joinActiveRoom(roomId, participantDetails)
+
+
+  //send information to users in room that they should prepare for incomming conn
+
+  roomDetails.participants.forEach(participant => {
+    if (participant.socketId !== participantDetails.socketId)
+    {
+      socket.to(participant.socketId).emit("conn-prepare", {
+        connUserSocketId:participantDetails.socketId
+      })
+      }
+  })
   roomUpdates.updateRooms();
 }
 
